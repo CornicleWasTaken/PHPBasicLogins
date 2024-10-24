7d997ecf656847e0a12cdf732ff9058ddb4729f9
Cornicle <137619136+CornicleWasTaken@users.noreply.github.com>
2024-10-25 03:54:47 +0530

    Add files via upload

---
+++ LoginRegister/script.js
@@ -0,0 +1,18 @@
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+const recoverButton = document.getElementById('recover');
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
+recoverButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
